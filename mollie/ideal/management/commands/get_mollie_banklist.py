326346fe336097198e9b9eff6578a3f3c5bca4a2
Maurits van Rees <maurits@vanrees.org>
2013-04-26 10:18:42 +0200

    Added --testmode option to get_mollie_banklist command.
    This gets the TBM Bank for testing.

--- mollie/ideal/management/commands/get_mollie_banklist.py
+++ mollie/ideal/management/commands/get_mollie_banklist.py
@@ -1,18 +1,32 @@
 #-*- coding: utf-8 -*-
 
 import os, urllib
+from optparse import make_option
 
-from django.core.management.base import NoArgsCommand, CommandError
+from django.core.management.base import BaseCommand, CommandError
 
 from mollie.ideal.settings import MOLLIE_API_URL
 
-class Command(NoArgsCommand):
+
+class Command(BaseCommand):
     help = 'Fetches the latest list of supported banks from Mollie.nl'
     requires_model_validation = False
+    option_list = BaseCommand.option_list + (
+        make_option(
+            '--testmode',
+            action='store_true',
+            dest='testmode',
+            default=False,
+            help=("Fetch the list in testmode.  This gets the test "
+                  "bank 'TBM Bank' (The Big Mollie Bank).")),
+        )
 
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
+        testmode = options['testmode']
         here = os.path.realpath('.')
         url = '%s?a=banklist' % MOLLIE_API_URL
+        if testmode:
+            url += '&testmode=true'
         file = os.path.join(here, 'mollie_banklist.xml')
         try:
             urllib.urlretrieve(url, file)
